19d3039171c2584ac94e958b98c11dd4ce174eb4
Vladimir Gulev <vladinson009@icloud.com>
2025-12-27 22:41:20 +0100

    Implemented: Landing Page

---
+++ src/app/(logged-out)/layout.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from 'react';
+type Props = PropsWithChildren & {};
+
+export default function LoggedOutLayout({ children }: Props) {
+  return (
+    <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
+      {children}
+    </div>
+  );
+}
---
+++ src/app/(logged-out)/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button';
+import { PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" />
+        SupportMe
+      </h1>
+      <p>The best dashboard to manage cusomer support</p>
+      <div className="flex gap-2 items-center">
+        <Button asChild className="cursor-pointer">
+          <Link href="login">Log in</Link>
+        </Button>
+        <small>or</small>
+        <Button asChild className="cursor-pointer" variant="outline">
+          <Link href="sign-up">Sing up</Link>
+        </Button>
+      </div>
+    </>
+  );
+}
