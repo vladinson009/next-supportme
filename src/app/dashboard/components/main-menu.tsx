bbe650314b2cf2593c276cc7008daf850dc30c2c
Vladimir Gulev <vladinson009@icloud.com>
2026-01-03 18:14:00 +0100

    Added: Dashboard main menu layout

--- src/app/dashboard/components/main-menu.tsx
+++ src/app/dashboard/components/main-menu.tsx
@@ -1,19 +1,33 @@
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import MenuItem from './menu-item';
 import MenuTitle from './menu-title';
+import Link from 'next/link';
+import LightDarkToggle from '@/components/light-dark-toggle';
 
 export default function MainMenu() {
   return (
-    <div className="bg-muted overflow-auto p-4">
-      <div className="border-b dark:border-b-black border-b-zinc-300 pb-4">
+    <nav className="bg-muted overflow-auto p-4 flex flex-col ">
+      <header className="border-b dark:border-b-black border-b-zinc-300 pb-4">
         <MenuTitle />
-      </div>
-      <div className="py-4">
+      </header>
+      <ul className="py-4 grow">
         <MenuItem href="/dashboard">My dashboard</MenuItem>
         <MenuItem href="/dashboard/teams">Teams</MenuItem>
         <MenuItem href="/dashboard/employees">Employees</MenuItem>
         <MenuItem href="/dashboard/account">Account</MenuItem>
         <MenuItem href="/dashboard/settings">Settings</MenuItem>
-      </div>
-    </div>
+      </ul>
+      <footer className="flex gap-2 items-center">
+        <Avatar>
+          <AvatarFallback className="bg-pink-300 dark:bg-pink-800">
+            VG
+          </AvatarFallback>
+        </Avatar>
+        <Link href="/" className="hover:underline">
+          Logout
+        </Link>
+        <LightDarkToggle className="ml-auto" />
+      </footer>
+    </nav>
   );
 }
--- src/app/dashboard/components/menu-item.tsx
+++ src/app/dashboard/components/menu-item.tsx
@@ -13,15 +13,17 @@ export default function MenuItem({ children, href }: PropsWithChildren<Props>) {
   const pathname = usePathname();
   const isActive = pathname === href;
   return (
-    <Link
-      className={cn(
-        'block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground',
-        isActive &&
-          'bg-primary hover:bg-primary dark:hover:bg-primary hover:text-foreground text-foreground'
-      )}
-      href={href}
-    >
-      {children}
-    </Link>
+    <li>
+      <Link
+        className={cn(
+          'block p-2 hover:bg-white dark:hover:bg-zinc-700 rounded-md text-muted-foreground hover:text-foreground',
+          isActive &&
+            'bg-primary hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground text-primary-foreground'
+        )}
+        href={href}
+      >
+        {children}
+      </Link>
+    </li>
   );
 }
