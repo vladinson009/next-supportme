ec01a4d2bb56d5fd1917974113c64053624a5ff4
Vladimir Gulev <vladinson009@icloud.com>
2026-01-03 23:54:43 +0100

    Implement: Dashboard link

--- src/app/(logged-out)/layout.tsx
+++ src/app/(logged-out)/layout.tsx
@@ -1,10 +1,20 @@
 import LightDarkToggle from '@/components/light-dark-toggle';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 type Props = PropsWithChildren & {};
 
 export default function LoggedOutLayout({ children }: Props) {
   return (
     <>
+      <div className="flex justify-center mt-10">
+        <Button asChild variant="link">
+          <Link className="animate-pulse " href="/dashboard">
+            <h1>Dashboard</h1>
+          </Link>
+        </Button>
+        <p>(Mocked backend)</p>
+      </div>
       <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
         {children}
       </div>
