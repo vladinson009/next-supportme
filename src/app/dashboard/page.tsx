b96688528b634cc9169b4095837098fb6e57e0c2
Vladimir Gulev <vladinson009@icloud.com>
2026-01-03 18:13:14 +0100

    Added: Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import EmployeesStats from './components/employees-stats';
+
 export default function DashboardPage() {
-  return <div>dashboard page</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
+        <TabsTrigger value="employees">Employees stats</TabsTrigger>
+        <TabsTrigger value="teams">Teams stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">
+        <EmployeesStats />
+      </TabsContent>
+      <TabsContent value="teams">Teams Stats View</TabsContent>
+    </Tabs>
+  );
 }
